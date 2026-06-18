0b78ff08cc995c3276288a434565a50960b23b36
conxztantino <emanoele.constantino@edu.ufes.br>
2026-06-18 10:13:35 -0300

    Add files via upload

---
+++ Scripts/AnchorStart.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class AnchorStart : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.AddComponent<ARAnchor>();
+    }
+}
