0d6a720ec31089bc726669df6c127c2eedef12a9
conxztantino <emanoele.constantino@edu.ufes.br>
2026-07-24 17:05:32 -0300

    Add files via upload

---
+++ Scripts/PointCollider.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PointCollider : MonoBehaviour
+{
+    public int Score;
+    public Text ScoreText;
+
+    void Start()
+    {
+       Time.timeScale = 1f; 
+    }
+
+    //void Update()
+}
